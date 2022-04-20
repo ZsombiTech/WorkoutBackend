88e665fd7d905362335524194537b15da504470c
ZsombiTech <horvathzsombor2005@gmail.com>
2022-04-20 03:04:31 +0200

    auth

--- controller/authController.js
+++ controller/authController.js
@@ -11,34 +11,30 @@ class Auth {
     };
 
     console.log("lefut");
-    UserModel.find(
-      { username: user.username },
-      "username",
-      async (err, docs) => {
-        if (docs.length > 0) {
-          console.log("lefut");
-          res.json({ response: "Already exits" });
-        } else {
-          console.log("lefut");
-          const post = new UserModel(user);
-          const savedPost = await post.save();
-          jwt.sign({ user }, "secretkey", async (err, token) => {
-            const usertoken = new TokenModel({ token });
-            const savedtoken = await usertoken.save();
-            res.json({ token });
-          });
-        }
+    UserModel.find({ email: user.email }, "username", async (err, docs) => {
+      if (docs.length > 0) {
+        console.log("lefut");
+        res.json({ response: "Already exits" });
+      } else {
+        console.log("lefut");
+        const post = new UserModel(user);
+        const savedPost = await post.save();
+        jwt.sign({ user }, "secretkey", async (err, token) => {
+          const usertoken = new TokenModel({ token });
+          const savedtoken = await usertoken.save();
+          res.json({ token: token, response: "correct" });
+        });
       }
-    );
+    });
   };
 
   login = async (req, res, next) => {
     const user = {
-      username: req.body.username,
+      email: req.body.email,
       password: req.body.password,
     };
 
-    UserModel.find({ username: user.username }, (err, docs) => {
+    UserModel.find({ email: user.email }, (err, docs) => {
       if (!err) {
         if (docs.length > 0) {
           if (user.password == docs[0].password) {
