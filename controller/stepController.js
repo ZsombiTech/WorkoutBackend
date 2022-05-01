1a78803956197e8f21c105c1629404af07e52d74
ZsombiTech <horvathzsombor2005@gmail.com>
2022-05-01 05:42:55 +0200

    new

--- controller/stepController.js
+++ controller/stepController.js
@@ -5,6 +5,7 @@ class StepController {
   getDailyStep = async (req, res, next) => {
     jwt.verify(req.token, "secretkey", async (err, authData) => {
       const username = req.body.username;
+      console.log(username);
       StepModel.find({ username: username }, (err, docs) => {
         const size = docs[0].steps.length;
         const date = new Date();
@@ -23,6 +24,37 @@ class StepController {
         } else {
           res.json("no");
         }
+      });
+    });
+  };
+  addDailyStep = async (req, res, next) => {
+    jwt.verify(req.token, "secretkey", async (err, authData) => {
+      const username = req.body.username;
+      const stepcount = req.body.stepcount;
+      const date = new Date();
+      const year = date.getUTCFullYear();
+      let day = date.getUTCDate();
+      let month = date.getUTCMonth() + 1;
+      if (month < 10) {
+        month = "0" + month;
+      }
+      if (day < 10) {
+        day = "0" + day;
+      }
+      const full = year + "-" + month + "-" + day;
+      StepModel.find({ username: username }, (err, docs) => {
+        let sizee = docs[0].steps.length;
+        let last = docs[0].steps[sizee - 1];
+        let id = last.id;
+        const newstep = {
+          id: id + 1,
+          step: stepcount,
+          date: full,
+        };
+        StepModel.findOneAndUpdate(
+          { username: username },
+          { $push: { steps: newstep } }
+        );
       });
     });
   };
